45e756a9d40c1f0874b2b295df4793eea5daa819
Shasank <shasank4u@gmail.com>
2012-10-02 13:26:37 +0530

    version 1.1
    adding controller class and content div and jar files

---
+++ src/com/accountingminds/web/FrontController.java
@@ -0,0 +1,12 @@
+package com.accountingminds.web;
+
+import javax.servlet.http.HttpServlet;
+
+public class FrontController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7069317426767683838L;
+
+}
